d407c73bf552abf632fa515675d3ccef43887411
Yunic10 <yudithnico99@gmail.com>
2024-01-14 19:50:59 +0700

    modify css & display kalkulator

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ LuasKelilingPersegi = () => {
     let sisi = document.getElementById('sisiPersegi').value;
     let luas = sisi*sisi;
     let keliling = sisi*4;
-    document.getElementById('luasPersegi').innerHTML = "<b><p>Luas : " + luas + " m2</p><p>Keliling : " + keliling + " m</p></b>";
-    document.getElementById('luasPersegi').style.display = "flex";
-    document.getElementById('luasPersegi').style.flexDirection = "column";
+    document.getElementById('luasKelilingPersegi').innerHTML = "<b><p>Luas : " + luas + " m2</p><p>Keliling : " + keliling + " m</p></b>";
+    document.getElementById('luasKelilingPersegi').style.display = "flex";
+    document.getElementById('luasKelilingPersegi').style.flexDirection = "column";
 }
 
 resetValue = () => {
     document.getElementById('sisiPersegi').value = null;
-    document.getElementById('luasPersegi').style.display = "none";
+    document.getElementById('luasKelilingPersegi').style.display = "none";
 }
